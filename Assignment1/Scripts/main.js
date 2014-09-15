bdb2828f6b0e87684cdb058fb77c68fcfc2a933d
ToddR83 <toddraymond_0@hotmail.com>
2014-09-14 21:19:12 -0400

    Added 4 additional HTML pages: About Me, Contact Me, Projects, and Services.  Also included Jquery

--- Assignment1/Scripts/main.js
+++ Assignment1/Scripts/main.js
@@ -1,1 +1,11 @@
-﻿
+﻿/// <reference path="jquery.min.js" />
+
+
+if (jQuery)
+{
+    $("#welcome-header").mouseover(function () {
+        $("#welcome-header").css("background-color", "grey");
+    });
+
+
+}
