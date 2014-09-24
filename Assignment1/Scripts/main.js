e49d913f573ef42419cf1558ee0d51881d3b111b
ToddR83 <toddraymond_0@hotmail.com>
2014-09-24 01:29:24 -0400

    Changed how my resume in "About.html" functions.  It is now a picture linking to the document for you to open and save as well as view.

--- Assignment1/Scripts/main.js
+++ Assignment1/Scripts/main.js
@@ -1,11 +1,1 @@
 ﻿/// <reference path="jquery.min.js" />
-
-
-if (jQuery)
-{
-    $("#welcome-header").mouseover(function () {
-        $("#welcome-header").css("background-color", "grey");
-    });
-
-
-}
